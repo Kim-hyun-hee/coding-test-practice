0d2675e2f608b5a0a2a3128b85ace59e94ebe007
HyunHee Kim <81128882+Kim-hyun-hee@users.noreply.github.com>
2025-06-19 22:08:35 +0900

    [level 1] Title: 둘만의 암호, Time: 0.46 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(string s, string skip, int index) {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string remain = string.Empty;
+            foreach (char c in alphabet)
+            {
+                if (!skip.Contains(c))
+                {
+                    remain = remain + $"{c}";
+                }
+            }
+
+            string answer = string.Empty;
+
+            foreach (char c in s)
+            {
+                var i = (remain.IndexOf(c) + index) % remain.Length;
+                answer += remain[i];
+            }
+
+            return answer;
+    }
+}
